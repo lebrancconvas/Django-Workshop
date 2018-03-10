f298c47e962d0f75d9b15fb06a65e85a7ad74e8c
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-11 03:13:57 +0700

    Edit Template Directory Setting.

--- musictest/musictest/settings.py
+++ musictest/musictest/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'musictest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['hellomusic/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

da928d1b2ee9b629d6de39793d15b71c53f2eaf5
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-11 00:28:13 +0700

    Edit HTTP Response.

--- musictest/hellomusic/views.py
+++ musictest/hellomusic/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello Music BNK48</h1>")
+    return HttpResponse(
+        """<h1>Hello Music</h1>
+           <h2>Music BNK48 Unofficial Fansite</h2>"""
+    )
